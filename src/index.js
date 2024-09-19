cf2f8d05ca9cf481422561b4e657a9de5a16fe40
Yalda Noorzad <yaldanoorzad05@gmail.com>
2024-09-19 17:27:09 +0430

    changed the language and added the API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-function generatepoem(event){
-    event.preventDefault();
+function generatepoem(response){
     new Typewriter("#poem", {
-        strings: "Mejor que todos los fuegos <br> que llaman artificiales",
+        strings: response.data.answer,
         autoStart: true,
-        delay: 7,
+        delay: 50,
         cursor: "",
     });
 
 }
+
+function makepoem(event){
+    event.preventDefault();
+    
+    let poeminput = document.querySelector("#poem-input")
+    let apikey = `f4850684o25d3207eb4aa74t2073f600`;
+    let prompt = `generate a Dari poem about ${poeminput.value}`;
+    let context = "generate a diffirent poem each time in four lines and seperate each line with <br /> try to be polie. at the end include `SheCodes AI` between a <strong></ strong> element and don't sign";
+    let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`
+    axios.get(apiurl).then(generatepoem);
+
+    new Typewriter("#poem", {
+        strings: `Generating the poem...`,
+        autoStart: true,
+        delay: 50,
+        cursor: "",
+    });
+
+}   
 let submit = document.querySelector("#form");
-submit.addEventListener("click", generatepoem);
+submit.addEventListener("submit", makepoem);
+
